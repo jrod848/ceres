7c93ee4861550aee807ee6ecdfb1e1c467f44c32
jrodr848 <rro.jorge@gmail>
2023-09-30 13:00:31 -0400

    Updated Login page, added Dashboard navigation functionality and graph.

--- script.js
+++ script.js
@@ -1,81 +1,111 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const authForm = document.getElementById("auth-form");
-  const errorMessage = document.getElementById("error-message");
-  const dashboard = document.getElementById("dashboard");
-  const logoutButton = document.getElementById("logout-button");
-  const options = document.querySelectorAll(".dashboard-option");
+const list = document.querySelectorAll('.list');
+const menuToggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+const loginBtn = document.getElementById("loginBtn");
+const loginForm = document.getElementById("loginForm");
+const loginBody = document.querySelector(".loginBody");
+const dashboardBody = document.querySelector(".dashboardBody");
+const logOutBtn = document.querySelector(".logOut");
+const errorMessage = document.getElementById("errorMessage");
+const graphContainer = document.querySelector(".graphContainer");
+const ctx = document.getElementById("chart1").getContext("2d");
 
-
-  function createChart(chartId, data) {
-    const ctx = document.getElementById(chartId).getContext("2d");
-    return new Chart(ctx, {
-        type: "line", // You can change the chart type (e.g., "bar", "pie", etc.)
-        data: data,
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-        },
-    });
+for (let i = 0; i < list.length; i++){
+    list[i].onclick = function() {
+        resetList();
+        setList(i);
+    } 
 }
 
-    options.forEach((option, index) => {
-        const graphContainer = option.querySelector(".graph-container");
-        const optionDetails = option.querySelector(".option-details");
-
-        option.addEventListener("click", function () {
-            // Toggle the display of the chart
-            if (graphContainer.style.display === "none" || graphContainer.style.display === "") {
-                optionDetails.style.display = "block";
-                graphContainer.style.display = "block";
-
-                // Create and update the chart
-                const chartData = {
-                    labels: ["Label 1", "Label 2", "Label 3"],
-                    datasets: [
-                        {
-                            label: "Data",
-                            data: [10, 20, 30],
-                            backgroundColor: "rgba(75, 192, 192, 0.2)",
-                            borderColor: "rgba(75, 192, 192, 1)",
-                            borderWidth: 1,
-                        },
-                    ],
-                };
-                createChart(`chart${index + 1}`, chartData);
-            } else {
-                optionDetails.style.display = "none";
-                graphContainer.style.display = "none";
-            }
-        });
-    });
-
-  authForm.addEventListener("submit", function (e) {
-      e.preventDefault();
-
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-
-      // You would typically send a request to a server for authentication here
-      // For simplicity, we're using a hardcoded username and password
-      if (email === "user@example.com" && password === "password") {
-          // Authentication successful
-          errorMessage.textContent = "";
-          authForm.style.display = "none";
-          dashboard.style.display = "block";
-          displayDashboard();
-      } else {
-          // Authentication failed
-          errorMessage.textContent = "Invalid email or password.";
-      }
-  });
-
-      logoutButton.addEventListener("click", function () {
-          // Clear the form inputs
-          document.getElementById("email").value = "";
-          document.getElementById("password").value = "";
-
-          // Hide the dashboard and show the login form
-          dashboard.style.display = "none";
-          authForm.style.display = "block";
-      });
+const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [
+        {
+          label: "Sunlight",
+          data: [],
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
 });
+
+
+menuToggle.onclick = function() {
+    if(navigation.style.display != "flex" 
+    || (navigation.style.display == "flex" && navigation.classList.contains('active') && breakpoint() =='md') 
+    || (navigation.style.display == "flex" && !navigation.classList.contains('active') && !menuToggle.classList.contains('active') && breakpoint() =='md')) {
+        navigation.style.display = "flex";
+    } else {
+    navigation.style.display = "none";
+    }
+    
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+loginBtn.onclick = function () {
+    resetList();
+    setList(0);
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email === "1" && password === "2") {
+        errorMessage.textContent = "";
+        loginBody.style.display = "none";
+        dashboardBody.style.display = "flex";
+    } else {
+        errorMessage.textContent = "Invalid email or password.";
+    }
+};
+
+logOutBtn.onclick = function () {
+    errorMessage.textContent = "";
+    loginBody.style.display = "flex";
+    dashboardBody.style.display = "none";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+};
+
+function resetList(){
+    let j = 0;
+    while(j<list.length){
+        list[j++].className = 'list';
+    }
+    
+}
+
+function setList(i){
+    list[i].className = 'list active';
+}
+
+function breakpoint() {
+    let breakpoints = {
+        '(min-width: 768px)': 'md',
+        '(min-width: 576px) and (max-width: 767.98px)': 'sm',
+        '(max-width: 575.98px)': 'xs',
+    }
+
+    for (let media in breakpoints) {
+        if (window.matchMedia(media).matches) {
+            return breakpoints[media];
+        }
+    }
+
+    return null;
+}
+
+setInterval(function() {
+    const now = new Date();
+    chart.data.labels.push(now.toLocaleTimeString());
+    chart.data.datasets[0].data.push(Math.random() * 2);
+    chart.update();
+  }, 5000);
